804c4085a62f396c6c539131b35300fe3829456b
Sxmourai <samourai_x@outlook.fr>
2024-03-15 16:45:02 +0100

    feature: removed plugin, only shell

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 """
 Calls the different users methods
+This should be called by the shell commands obsidian plugin
 """
 import user
-import sys
+import sys, inspect
 
-args = sys.argv[1:]
 
-command = args[0]
-
-if command == "get-funcs":
+def print_funcs():
     for name, func in user.__dict__.items():
         if callable(func):
             print(name)
-elif command == "exec":
-    for name, func in user.__dict__.items():
-        if callable(func) and args[1] == name:
-            print(help(func))
-            func()
-else:
-    print("Invalid command")
+
+args = sys.argv[1:]
+user_command = args[0].split(" ")
+user_command, user_args = user_command[0], user_command[1:]
+
+if user_command == "get-funcs":
+    print_funcs()
+    exit()
+
+for name, func in user.__dict__.items():
+    if callable(func) and name.lower().startswith(user_command.lower()):
+        try:
+            func(*user_args)
+            exit(0)
+        except TypeError as e:
+            print(f"Type error: {e}\n\nMaybe you forgot to add cli arguments to YOUR function (arguments are passed: *sys.args[3:])")
+            exit(2)
+
+print(f"Command not found {user_command} with args {user_args} !\nAvailable commands: ")
+print_funcs()
+exit(2)
--- user.py
+++ user.py
@@ -1,2 +1,8 @@
 def example(str):
     print(f"hi {str} !")
+
+
+def lot_of_args(a1, a2, *args):
+    print(f"1{a1} 2{a2} Other:{args}")
+
+    
